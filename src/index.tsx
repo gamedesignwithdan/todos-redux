00b333ebf51665b28e1c84ee165cd44c26e7b2f8
Daniel <gamedesignwithdan@gmail.com>
2021-04-12 12:04:01 +0100

    introducing redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React/*, { useState }*/ from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers/index'
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// function App(props: AppProps) {
+//     const [counter, setCount] = useState(0);
+//     const onIncrement = () => setCount(counter + 1)
+//     const onDecrement = () => setCount(counter - 1)
+
+//     return (
+//         <div>
+//             <button onClick={onIncrement}>Increment</button>
+//             <button onClick={onDecrement}>Decrement</button>
+//             {counter}
+//         </div>
+//     )
+// }
